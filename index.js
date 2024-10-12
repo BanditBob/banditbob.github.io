03ccacb610c15752a7758393d5e3cbfcb314a5e4
BanditBob <brandon.bennett.ai@gmail.com>
2024-10-12 16:34:11 -0600

    Added Icons, and started to write a basic jQuery script

---
+++ index.js
@@ -0,0 +1,65 @@
+// window.onload = function () {
+//     alert("It freakin worked!");
+// }
+
+// (() => {
+let logos = {
+    web_dev: [
+        { n: "angularjs", v: "original" },
+        { n: "axios", v: "plain" },
+        { n: "bootstrap", v: "original" },
+        { n: "css3", v: "original" },
+        { n: "html5", v: "original" },
+        { n: "javascript", v: "original" },
+        { n: "jquery", v: "original" },
+        { n: "json", v: "original" },
+        { n: "npm", v: "original" },
+        { n: "nodejs", v: "original" },
+        { n: "typescript", v: "original" },
+    ],
+    tools: [
+        { n: "bash", v: "original" },
+        { n: "firefox", v: "original" },
+        { n: "git", v: "original" },
+        { n: "linux", v: "original" },
+        { n: "powershell", v: "original" },
+        { n: "slack", v: "original" },
+        { n: "ubuntu", v: "original" },
+        { n: "vscode", v: "original" },
+    ],
+    data_analysis: [
+        { n: "jupyter", v: "original" },
+        { n: "numpy", v: "original" },
+        { n: "pandas", v: "original" },
+        { n: "playwright", v: "original" },
+        { n: "plotly", v: "original" },
+        { n: "python", v: "original" },
+        { n: "visualbasic", v: "original" },
+    ],
+};
+//     let new_elems = [];
+//     for (let key in logos) {
+//         console.log(`${key}: ${logos[key]}`)
+//     }
+//     console.log(document.getElementById("skills"));
+// })();
+
+
+$(document).ready(() => {
+    console.clear();
+    // console.log($("#skills").children());
+    const skills = $("#skills");
+
+    console.log(skills.children())
+
+    for (let k in logos) {
+        logos[k].forEach(el => {
+            console.log(el.n + "-" + el.v)
+            $("<img height='48' width='48'/>")
+                // .attr("src", `https://cdn.jsdeliver.net/gh/devicons/devicons@latest/icons/${el.n}/${el.n}-${el.v}.svg`)
+                .appendTo(skills);
+        });
+        $("<hr />").appendTo(skills);
+    }
+
+});
